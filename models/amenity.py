de63a9d81513ed421b8694783a6b8f3f5e932007
basma ebraheim <basmaebraheim@gmail.com>
2024-04-30 23:21:02 +0300

    use mysql

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
-from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy import Column, Table, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 class Amenity(BaseModel, Base):
     """the class for Amenity
--- models/place.py
+++ models/place.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.amenity import Amenity
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
 from models.base_model import BaseModel, Base
-
-
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+from models.amenity import place_amenity
 
 
 class Place(BaseModel, Base):
